b407c37e03aa32ad2ac7e36a37e4757270965093
Ola Theander <ola.theander@myola.se>
2021-11-11 23:15:12 +0100

    Added Spring Boot Actuator, docker-compose for Kafka and a simple REST endpoint.

--- src/main/java/com/example/springkafkademo/HelloController.java
+++ src/main/java/com/example/springkafkademo/HelloController.java
@@ -1,7 +1,15 @@
 package com.example.springkafkademo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
+
+    @GetMapping("/greeting")
+    public String greeting(String name) {
+        return "Hello " + name;
+    }
 }
